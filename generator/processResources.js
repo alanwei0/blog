83ee6e8401061d3de44360b4bd5bbd54431d9711
Alan <alanwei000@gmail.com>
2020-12-11 00:14:30 +0800

    feat: add favicon & deploy dist to gh-page script

--- generator/processResources.js
+++ generator/processResources.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 exports.processResources = async ({ DIST_DIR, CLIENT_ROOT }) => {
   const copyCss = fse.copy(path.join(CLIENT_ROOT, 'css'), path.join(DIST_DIR, 'css'))
-  const copyIcons = fse.copy(path.join(CLIENT_ROOT, 'icons'), path.join(DIST_DIR, 'icons'))
+  const copyIcons = fse.copy(path.join(CLIENT_ROOT, 'images'), path.join(DIST_DIR, 'images'))
   const copyJs = fse.copy(path.join(CLIENT_ROOT, 'js'), path.join(DIST_DIR, 'js'))
 
   return Promise.all([copyCss, copyJs, copyIcons])
